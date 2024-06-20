4ca397dfafc07b12aff0bee643711a35b69eb64a
itsmesanjana <kanakisanjana@gmail.com>
2024-06-20 21:38:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import LoginSignUp from './Components/LoginSignUp/LoginSignup';
+
+function App() {
+  return (
+    <div >
+      <LoginSignUp/>
+    </div>
+  );
+}
+
+export default App;
